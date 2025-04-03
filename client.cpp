33cc117bc06819d87affb0181a677f5c8c164a98
itzzuy <duythe0210@gmail.com>
2025-04-03 15:30:47 -0400

    add file

---
+++ client.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <string>
+
+#define PORT 8000
+#define BUFFER_SIZE 1024
+#define IP "127.0.0.1"
+
+#pragma comment(lib, "ws2_32.lib")
+
+void error_exit(const char* message) {
+    std::cerr << message << "Error Code: " << WSAGetLastError() << std::endl;
+    WSACleanup();
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
+    exit(EXIT_FAILURE);
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET clientSocket;
+    sockaddr_in serverAddr;
+    char buffer[BUFFER_SIZE];
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        error_exit("WSAStartup Failed");
+    }
+
+    // Create socket
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        error_exit("Socket creation failed");
+    }
+
+    // Setup server address
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, IP, &serverAddr.sin_addr) <= 0) {
+        error_exit("Invalid address");
+    }
+
+    // Connect to server
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        error_exit("Connection failed");
+    }
+    std::cout << "Connected to server!" << std::endl;
+
+    std::string message = "Hello from client!";
+    if (send(clientSocket, message.c_str(), message.size(), 0) == SOCKET_ERROR) {
+        error_exit("Send failed");
+    }
+    std::cout << "Message sent to server: " << message << std::endl;
+
+    // Receive a message from the server
+    int bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+    if (bytesReceived > 0) {
+        buffer[bytesReceived] = '\0';
+        std::cout << "Message received from server: " << buffer << std::endl;
+    }
+    else {
+        std::cerr << "Failed to receive message from server" << std::endl;
+    }
+
+    std::cout << "Connected to server!" << std::endl;
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
